38e7d5b56a8accfa72565d7e25addf7c1fde86ce
likhe <likhelifi@laposte.net>
2017-01-05 22:46:44 +0100

    add script js modify all files - correct my css and my js

--- app.js
+++ app.js
@@ -1,64 +1,104 @@
-//1. appel du DOC html
-$(document).ready(function() {
-    // 2. define all variables we need after
-    var $slide = $('.slide'), // target id slide
-        $img = $('.slide img'), // target img inside the id slide
-        indexImg = $img.length - 1, // define the index of the last element
-        i = 0, // countdown to 0
-        $currentImg = $img.eq(i);
-    /* target the current index i in position 0
-    eq(index/position) is in Js translation the queryselector()*/
-    //3. hide all img except the current img - manipulate the css
-    $img.css('display', 'none'); // hide img
-    $currentImg.css('display', 'inline-block'); // post only the current img
-    //4. manage the page change from an image to the next img
-    //4.1manipulate the html - add the prev and next direction
-    $slide.append('<div class="controls"> <span class="prev">&#65513;</span> <span class="next">&#xffeb;</span> </div>');
-    //4.2 call the class span next
-    // next img
-    $('.next').click(function() {
-        i++; //add countdown
-        $img.css('display', 'none'); // hide img
-        $currentImg = $img.eq(i); // define new img
-        $currentImg.css('display', 'inline-block'); // then post it
-    });
-     // previous img
-    $('.prev').click(function() {
-        i--; // return the countdown
-        $img.css('display', 'none');//hide img
-        $currentImg = $img.eq(i);//define new img
-        $currentImg.css('display', 'inline-block'); //then post it
-    });
-})
-// NB: arrows work but when there is no img left continue to send blank
-//5. Limit the img display only with
-$('.next').click(function(){ // next img
+//1. set ul width 
+//2. image when click prev/next button
+var ul;
+var liItems;
+var imageNumber;
+var imageWidth;
+var prev, next;
+var currentPostion = 0;
+var currentImage = 0;
 
-    i++; //add countdown
-//if i is inferior or equal to indexImg...
-    if( i <= indexImg ){
-        $img.css('display', 'none'); // hide me img
-        $currentImg = $img.eq(i); // define the new img
-        $currentImg.css('display', 'inline-block'); // post-it
-    }
-//...else
-    else{
-        i = indexImg;
-    }
 
-});
+function init(){
+	ul = document.getElementById('image_slider');
+	liItems = ul.children;
+	imageNumber = liItems.length;
+	imageWidth = liItems[0].children[0].clientWidth;
+	ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
+	prev = document.getElementById("prev");
+	next = document.getElementById("next");
+	generatePager(imageNumber);
+	//.onclike = slide(-1) will be fired when onload;
+	/*
+	prev.onclick = function(){slide(-1);};
+	next.onclick = function(){slide(1);};*/
+	prev.onclick = function(){ onClickPrev();};
+	next.onclick = function(){ onClickNext();};
+}
 
-$('.prev').click(function(){ // previous img
+function animate(opts){
+	var start = new Date;
+	var id = setInterval(function(){
+		var timePassed = new Date - start;
+		var progress = timePassed / opts.duration;
+		if (progress > 1){
+			progress = 1;
+		}
+		var delta = opts.delta(progress);
+		opts.step(delta);
+		if (progress == 1){
+			clearInterval(id);
+			opts.callback();
+		}
+	}, opts.delay || 17);
+	//return id;
+}
 
-    i--; // remove countdown then do same thing as next
+function slideTo(imageToGo){
+	var direction;
+	var numOfImageToGo = Math.abs(imageToGo - currentImage);
+	// slide toward left
 
-    if( i >= 0 ){
-        $img.css('display', 'none');
-        $currentImg = $img.eq(i);
-        $currentImg.css('display', 'inline-block');
-    }
-    else{
-        i = 0;
-    }
+	direction = currentImage > imageToGo ? 1 : -1;
+	currentPostion = -1 * currentImage * imageWidth;
+	var opts = {
+		duration:1000,
+		delta:function(p){return p;},
+		step:function(delta){
+			ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
+		},
+		callback:function(){currentImage = imageToGo;}	
+	};
+	animate(opts);
+}
 
-});
+function onClickPrev(){
+	if (currentImage == 0){
+		slideTo(imageNumber - 1);
+	} 		
+	else{
+		slideTo(currentImage - 1);
+	}		
+}
+
+function onClickNext(){
+	if (currentImage == imageNumber - 1){
+		slideTo(0);
+	}		
+	else{
+		slideTo(currentImage + 1);
+	}		
+}
+
+function generatePager(imageNumber){	
+	var pageNumber;
+	var pagerDiv = document.getElementById('pager');
+	for (i = 0; i < imageNumber; i++){
+		var li = document.createElement('li');
+		pageNumber = document.createTextNode(parseInt(i + 1));
+		li.appendChild(pageNumber);
+		pagerDiv.appendChild(li);
+		// add event inside a loop, closure issue.
+		li.onclick = function(i){
+			return function(){
+				slideTo(i);
+			}
+		}(i);
+	}	
+	var computedStyle = document.defaultView.getComputedStyle(li, null);
+	//border width 1px; offsetWidth = 22
+	var liWidth = parseInt(li.offsetWidth);
+	var liMargin = parseInt(computedStyle.margin.replace('px',""));
+	pagerDiv.style.width = parseInt((liWidth + liMargin * 2) * imageNumber) + 'px';
+}
+window.onload = init;
