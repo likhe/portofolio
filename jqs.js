3008af90489dc48a5111c35e66a09d9d869e182f
likhe <likhelifi@laposte.net>
2017-01-05 15:04:45 +0100

    jq change img with prec and suiv - viewport to img - change css in jq to center img

--- jqs.js
+++ jqs.js
@@ -1,5 +1,5 @@
 //1. appel du DOC html
-$(document).ready(function() {/*
+$(document).ready(function() {
     // 2. define all variables we need after
     var $slide = $('.slide'), // target id slide
         $img = $('.slide img'), // target img inside the id slide
@@ -7,10 +7,9 @@ $(document).ready(function() {/*
         i = 0, // countdown to 0
         $currentImg = $img.eq(i);
     /* target the current index i in position 0
-           eq(index/position) is in Js translation the queryselector()
-           */
+    eq(index/position) is in Js translation the queryselector()*/
     //3. hide all img except the current img - manipulate the css
-    /*$img.css('display', 'none'); // hide img
+    $img.css('display', 'none'); // hide img
     $currentImg.css('display', 'block'); // post only the current img
     //4. manage the page change from an image to the next img
     //4.1manipulate the html - add the prev and next direction
@@ -29,5 +28,5 @@ $(document).ready(function() {/*
         $img.css('display', 'none');//hide img
         $currentImg = $img.eq(i);//define new img
         $currentImg.css('display', 'block'); //then post it
-    });*/
+    });
 })
