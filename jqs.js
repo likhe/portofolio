8ccd37e8161bf377295bec08d55e359e0d9ec898
likhe <likhelifi@laposte.net>
2017-01-05 12:41:58 +0100

    correct css by julien - pb came from size not defined in img

--- jqs.js
+++ jqs.js
@@ -1,14 +1,33 @@
-//appel du DOC html
-$(document).ready(function() {
-    // define all variables we need after
-    var $slide = $('#slide'), // target id slide
-        $img = $('#slide img'), // target img inside the id slide
+//1. appel du DOC html
+$(document).ready(function() {/*
+    // 2. define all variables we need after
+    var $slide = $('.slide'), // target id slide
+        $img = $('.slide img'), // target img inside the id slide
         indexImg = $img.length - 1, // define the index of the last element
         i = 0, // countdown to 0
-        $currentImg = $img.eq(i); /* target the current index i in position 0
-        eq(index/position) is in Js translation the queryselector
-        */
-    $img.css('display', 'none'); // on cache les images
-    $currentImg.css('display', 'block'); // on affiche seulement l'image courante
-
+        $currentImg = $img.eq(i);
+    /* target the current index i in position 0
+           eq(index/position) is in Js translation the queryselector()
+           */
+    //3. hide all img except the current img - manipulate the css
+    /*$img.css('display', 'none'); // hide img
+    $currentImg.css('display', 'block'); // post only the current img
+    //4. manage the page change from an image to the next img
+    //4.1manipulate the html - add the prev and next direction
+    $slide.append('<div class="controls"> <span class="prev">Precedent</span> <span class="next">Suivant</span> </div>');
+    //4.2 call the class span next
+    // next img
+    $('.next').click(function() {
+        i++; //add countdown
+        $img.css('display', 'none'); // hide img
+        $currentImg = $img.eq(i); // define new img
+        $currentImg.css('display', 'block'); // then post it
+    });
+     // previous img
+    $('.prev').click(function() {
+        i--; // return the countdown
+        $img.css('display', 'none');//hide img
+        $currentImg = $img.eq(i);//define new img
+        $currentImg.css('display', 'block'); //then post it
+    });*/
 })
