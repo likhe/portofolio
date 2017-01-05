c035d2e8630d075db7c21f9eb96d481aefe2bcc0
likhe <likhelifi@laposte.net>
2017-01-05 15:52:50 +0100

    jq complete the slideshow - see after how to style it - css : what's wrong with my img

--- jqs.js
+++ jqs.js
@@ -20,13 +20,45 @@ $(document).ready(function() {
         i++; //add countdown
         $img.css('display', 'none'); // hide img
         $currentImg = $img.eq(i); // define new img
-        $currentImg.css('display', 'block'); // then post it
+        $currentImg.css('display', 'inline-block'); // then post it
     });
      // previous img
     $('.prev').click(function() {
         i--; // return the countdown
         $img.css('display', 'none');//hide img
         $currentImg = $img.eq(i);//define new img
-        $currentImg.css('display', 'block'); //then post it
+        $currentImg.css('display', 'inline-block'); //then post it
     });
 })
+// NB: arrows work but when there is no img left continue to send blank
+//5. Limit the img display only with
+$('.next').click(function(){ // next img
+
+    i++; //add countdown
+//if i is inferior or equal to indexImg...
+    if( i <= indexImg ){
+        $img.css('display', 'none'); // hide me img
+        $currentImg = $img.eq(i); // define the new img
+        $currentImg.css('display', 'inline-block'); // post-it
+    }
+//...else
+    else{
+        i = indexImg;
+    }
+
+});
+
+$('.prev').click(function(){ // previous img
+
+    i--; // remove countdown then do same thing as next
+
+    if( i >= 0 ){
+        $img.css('display', 'none');
+        $currentImg = $img.eq(i);
+        $currentImg.css('display', 'inline-block');
+    }
+    else{
+        i = 0;
+    }
+
+});
