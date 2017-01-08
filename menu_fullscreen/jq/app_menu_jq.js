c554a7a91fbe440859ad6b59e5cab1f51188d8db
likhe <likhelifi@laposte.net>
2017-01-08 08:08:54 +0100

    test jq on logo works

--- menu_fullscreen/jq/app_menu_jq.js
+++ menu_fullscreen/jq/app_menu_jq.js
@@ -1,8 +1,7 @@
 $(function() {
-	//add the selector nav-icon
-//when you click on the nav icon class, i want the nav_opened class to be opened.
-	$(".nav-icon").click(function() {
-//toggleClass:Add or remove one or more classes from each element in the set of matched elements, depending on either the class's presence or the value of the state argument.
+	//when you click on the nav icon class or the nav overlay class, i want the nav_opened class to be opened.
+	$(".nav-icon, .nav-overlay").click(function() {
+		//toggleClass:Add or remove one or more classes from each element in the set of matched elements, depending on either the class's presence or the value of the state argument.
 		$(".header").toggleClass("nav-opened")
 	})
 });
