961d2a0991fc21cf2576b9c6d963200f5774edc3
likhe <likhelifi@laposte.net>
2017-01-17 16:13:38 +0100

    add javascript event load, scroll and detect viewport - add class in li

--- timeline/app_time.js
+++ timeline/app_time.js
@@ -1,26 +1,33 @@
 "use strict";
 
 /**a DOM element can be visible in the current viewport*******************/
-//create a function whiwh can detect the viewport*/
-function isVisibleInViewport(el) {
-//offsetTop: 
-    let top = el.offsetTop,
-        left = el.offsetLeft,
-        width = el.offsetWidth,
-        height = el.offsetHeight;
 
-    while (el.offsetParent) {
-        el = el.offsetParent;
-        top += el.offsetTop;
-        left += el.offsetLeft;
+/*By default, the timeline elements (divs) should be hidden.
+They should appear when their parent (list item) enters the viewport.
+The first task is relatively straightforward. The second one, though,
+is a bit more complicated. We need to detect if the target elements (list items)*/
+
+//1. create a function test whether an element is visible in the current viewport or not.
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+    //2. TRhe list is visible When the page loads & As we scroll down
+    var items = document.querySelectorAll(".timeline li");
+
+    // code for the isElementInViewport function
+
+    function callbackFunc() {
+        for (var i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+                items[i].classList.add("in-view");
+            }
+        }
     }
 
-    return (
-        top < (window.pageYOffset + window.innerHeight) &&
-        left < (window.pageXOffset + window.innerWidth) &&
-        (top + height) > window.pageYOffset &&
-        (left + width) > window.pageXOffset
-    );
-}
-
-}
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
