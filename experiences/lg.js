7a3d1f6054f632c52a40bc45ebf3a4fbe92952ab
likhe <likhelifi@laposte.net>
2017-01-30 22:23:53 +0100

    fix padding and margin space of div timeline - add hidden nav jq

--- experiences/lg.js
+++ experiences/lg.js
@@ -1,38 +1,41 @@
 "use strict";
-/***********a var object with 2 other objects******************/
-let translationArr = {
-    'en' : {
-        'cv': "Resume",
-        'job':"Web integrator",
-        'pj': "Works",
-        'legal': "Mentions",
-        'contact': "Contact"
-    },
-    'fr' : {
-        'cv': "Curriculum Vitae",
-        'pj': "Portfolio",
-        'job': "Intégratrice web",
-        'legal': "Mentions",
-        'contact': "Contact"
-    },
-    'pt' : {
-      'cv': "Curricolo",
-      'pj': "Portfolio",
-      'job': "Integradora de web",
-      'legal': "Mençoes legais",
-      'contact': "Contacto"
-    }
-};
-/**************loop lang elements to each item when element translate class is clicked***************/
-$(function() {
-    /*****create an event onclick******************/
-    $('.translate').click(function() {
-        /**********create a var*************/
-        var languages = $(this).attr('id');
-        /**********target the class lang for the change*************/
-        $('.lang').each(function(index, element) {
-          $(this).text(translationArr[languages][$(this).attr('key')]);
+$(document).ready(function() {
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    });
+    /***********a var object with 2 other objects******************/
+    let translationArr = {
+        'en': {
+            'cv': "Resume",
+            'pj': "Works",
+            'legal': "Mentions",
+            'contact': "Contact"
+        },
+        'fr': {
+            'cv': "Curriculum Vitae",
+            'pj': "Portfolio",
+            'legal': "Mentions",
+            'contact': "Contact"
+        },
+        'pt': {
+            'cv': "Curricolo",
+            'pj': "Portfolio",
+            'legal': "Mençoes legais",
+            'contact': "Contacto"
+        }
+    };
+    /**************loop lang elements to each item when element translate class is clicked***************/
+    $(function() {
+        /*****create an event onclick******************/
+        $('.translate').click(function() {
+            /**********create a var*************/
+            var languages = $(this).attr('id');
+            /**********target the class lang for the change*************/
+            $('.lang').each(function(index, element) {
+                $(this).text(translationArr[languages][$(this).attr('key')]);
 
+            });
         });
     });
 });
