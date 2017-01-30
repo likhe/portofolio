655a9837158c9301fb56e3f744bb4d54b25dcc13
likhe <likhelifi@laposte.net>
2017-01-30 23:26:11 +0100

    translate into en and pt - adjust size of some text - reduce padding of piechart

--- responsive_piechart/app.js
+++ responsive_piechart/app.js
@@ -1,6 +1,50 @@
-$(document).ready(function () {
-$('#toggle').click(function() {
-   $(this).toggleClass('active');
-   $('#overlay').toggleClass('open');
-  });
- });
+"use strict";
+$(document).ready(function() {
+    $('#toggle').click(function() {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    });
+    /***********a var object with 2 other objects******************/
+    let translationArr = {
+        'fr': {
+            'home': "Accueil",
+            'cv': "Présentation",
+            'pj': "Portfolio",
+            'contact': "Contact",
+            "inf":'Infographie',
+            'fs':"Veille stratégique",
+            'legal': "Mentions légales"
+        },
+        'en': {
+            'home': "Home",
+            'cv': "Resume",
+            'pj': "Works",
+            'contact': "Contact",
+            "inf":'Infographic',
+            'fs':"Strategic foresight",
+            'legal': "Mentions"
+        },
+        'pt': {
+            'home': "Início",
+            'cv': "Currículo",
+            'pj': "Portfolio",
+            'contact': "Contato",
+            "inf":'Infografia',
+            'fs':"Prospecção estratégica",
+            'legal': "Menções legais"
+        }
+    };
+    /**************loop lang elements to each item when element translate class is clicked***************/
+    $(function() {
+        /*****create an event onclick******************/
+        $('.translate').click(function() {
+            /**********create a var*************/
+            var languages = $(this).attr('id');
+            /**********target the class lang for the change*************/
+            $('.lang').each(function(index, element) {
+                $(this).text(translationArr[languages][$(this).attr('key')]);
+
+            });
+        });
+    });
+});
