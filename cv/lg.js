4755e854c86624e760152f676e233c4b2623d779
likhe <likhelifi@laposte.net>
2017-02-02 17:30:56 +0100

    correct layout files

--- cv/lg.js
+++ cv/lg.js
@@ -1,23 +1,29 @@
 "use strict";
 /***********a var object with 2 other objects******************/
 let translationArr = {
-    'en' : {
-        'cv': "Resume",
-        'pj': "Works",
-        'legal': "Mentions",
-        'contact': "Contact"
-    },
-    'fr' : {
+    'fr': {
         'cv': "Curriculum Vitae",
         'pj': "Portfolio",
+        'introduce': "Presentation",
+        'exp': "Experiences",
+        'timeline': "Parcours",
         'legal': "Mentions",
         'contact': "Contact"
     },
-    'pt' : {
-      'cv': "Curricolo",
-      'pj': "Portfolio",
-      'legal': "Mençoes legais",
-      'contact': "Contacto"
+    'en': {
+        'cv': "Resume",
+        'pj': "Works",
+        'introduce': "About me",
+        'exp': "Skills",
+        'timeline': "Timeline",
+        'legal': "Mentions",
+        'contact': "Contact"
+    },
+    'pt': {
+        'cv': "Curricolo",
+        'pj': "Portfolio",
+        'legal': "Mençoes legais",
+        'contact': "Contacto"
     }
 };
 /**************loop lang elements to each item when element translate class is clicked***************/
@@ -28,7 +34,7 @@ $(function() {
         var languages = $(this).attr('id');
         /**********target the class lang for the change*************/
         $('.lang').each(function(index, element) {
-          $(this).text(translationArr[languages][$(this).attr('key')]);
+            $(this).text(translationArr[languages][$(this).attr('key')]);
 
         });
     });
